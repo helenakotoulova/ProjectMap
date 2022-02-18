70ce85f93c539cfc7f23c28d85cf1d8baf5771ae
helena <kotoulova.helena@gmail.com>
2022-02-18 14:35:49 +0100

    initial commit

---
+++ src/data/data.js
@@ -0,0 +1,50 @@
+export const data = [
+  {
+    id: 1,
+    title: "Expenses",
+    link: "https://expensesproject-db7c3.web.app/",
+    image: "./images/expenses.png",
+  },
+  {
+    id: 2,
+    title: "Tasks",
+    link: "https://ukoly-d268c.web.app",
+    image: "./images/tasks.png",
+  },
+  {
+    id: 3,
+    title: "Grocery Bud",
+    link: "https://grocerybud-41405.web.app",
+    image: "./images/grocery.png",
+  },
+  {
+    id: 4,
+    title: "Stripe Menu",
+    link: "https://stripemenu.web.app/",
+    image: "./images/stripeMenu.png",
+  },
+  {
+    id: 5,
+    title: "Cocktails",
+    link: "https://cocktails-d6109.web.app",
+    image: "./images/cocktails.png",
+  },
+  {
+    id: 6,
+    title: "Color Generator",
+    link: "https://colorgenerator-15ed2.web.app/",
+    image: "./images/colorGenerator.png",
+  },
+  {
+    id: 7,
+    title: "Slider",
+    link: "https://slider-a3916.web.app",
+    image: "./images/slider.png",
+  },
+  {
+    id: 8,
+    title: "Tours",
+    link: "https://tours-df023.web.app",
+    image: "./images/tours.png",
+  },
+];
